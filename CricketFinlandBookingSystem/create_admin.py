ee04bf095f4be67eae57acc06babfa3659f8e475
kumarx <legspincric@gmail.com>
2024-05-07 09:07:41 +0300

    changes7thmay

--- CricketFinlandBookingSystem/create_admin.py
+++ CricketFinlandBookingSystem/create_admin.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
 
 # Setup Flask and SQLAlchemy
+load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./bookings.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -15,7 +17,8 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(120), unique=True, nullable=False)  # Ensure this line is present
+    password_hash = db.Column(db.String(512))
     role = db.Column(db.String(64))
 
     def set_password(self, password):
@@ -25,11 +28,12 @@ def create_admin():
     with app.app_context():
         username = 'admin'
         password = 'cricketfinland23'  # Use a strong, unique password for production
+        email = 'tdk.avashesh@yahoo.com'
         role = 'admin'
 
         existing_admin = User.query.filter_by(username=username).first()
         if not existing_admin:
-            admin_user = User(username=username, role=role)
+            admin_user = User(username=username, email=email, role=role)
             admin_user.set_password(password)
             db.create_all()  # Ensure all tables are created
             db.session.add(admin_user)
